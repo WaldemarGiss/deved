5dc9d89081a445e6dae68442d92f7a415079d4d9
WGiss <w.giss@gmx.de>
2020-02-20 08:34:23 +0100

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const User = require('../model/User');
+
+//Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string()
+        .min(6)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: Joi.string()
+        .min(6)
+        .required()
+});
+
+router.post('/register', async (req, res) => {
+
+    //Validate before adding user
+    const {value, error} = await schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    const savedUser = await user.save();
+
+    res.send(savedUser);
+});
+
+module.exports = router;
+
+
